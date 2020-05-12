d6d26745d49b9088e4a0cf27fb104b7188422087
vincentlieu <v.lieu93@gmail.com>
2020-05-12 12:23:16 +1000

    HRBC-23 - generated models for Brand, Category and Condition

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,21 @@
-user_count = User.count
+def total_count()
+  #Total counts
+  user_count = User.count
+  category_count = Category.count
+  brand_count = Brand.count
+  condition_count = Condition.count
+
+  p "========================================"
+  p "TOTAL USERS CREATED: #{user_count}"
+  p "TOTAL CLOTHING CATEGORIES CREATED: #{category_count}"
+  p "TOTAL BRANDS CREATED: #{brand_count}"
+  p "TOTAL CONDITIONS CREATED: #{condition_count}"
+  p "========================================"
+end
+
+clothing_category = ["Accessories", "Bottoms", "Headwear", "Outerwear", "Shirt", "T-Shirt", "Bags"]
+brands = ["Adidas", "Nike", "Supreme", "Off-White", "Palace", "Bape", "Kaws"]
+conditions = ["New with tags", "New without tags", "New with box", "New without box", "New with defects", "Pre-owned"]
 
 # Generate test account
 User.create(
@@ -18,9 +35,24 @@ for i in 1..10
     password_confirmation: "password",
     biography: Faker::Lorem.sentence(word_count: 3),
   )
-  p "#{i} Random account created."
 end
 
-p "========================================"
-p "TOTAL USER CREATED: #{user_count}"
-p "========================================"
+clothing_category.each do |category|
+  Category.create(
+    name: category,
+  )
+end
+
+brands.each do |brand|
+  Brand.create(
+    name: brand,
+  )
+end
+
+conditions.each do |condition|
+  Condition.create(
+    name: condition,
+  )
+end
+
+total_count()
