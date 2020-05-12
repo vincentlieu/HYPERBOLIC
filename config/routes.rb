296263b67003c81179f5acf087319a0b6322ea12
vincentlieu <v.lieu93@gmail.com>
2020-05-12 16:35:54 +1000

    HRBC-25 - Created listing routes, controllers and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "/", to: "home#index", as: "root"
+  get "/", to: "pages#home", as: "root"
+  get "about", to: "pages#about", as: "about"
+  get "help", to: "pages#help", as: "help"
+  get "listings", to: "listings#index", as: "listings"
+  post "listings", to: "listings#create"
+  get "listings/new", to: "listings#new", as: "new_listing"
+  get "listings/:id", to: "listings#show", as: "listing"
+  get "listings/:id/edit", to: "listings#edit", as: "edit_listing"
+
+  patch "/listings/:id", to: "listings#update"
+  put "/listings/:id", to: "listings#update"
+  delete "/listings/:id", to: "listings#destroy"
 end
