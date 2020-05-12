5aa46401e940fcc6abdae408c1c852442403629b
vincentlieu <v.lieu93@gmail.com>
2020-05-12 23:18:47 +1000

    HRBC-7 - When the user deletes their account the listing associated are also removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
-  has_many :listings
+  has_many :listings, dependent: :destroy
   validates :full_name, presence: true
 end
