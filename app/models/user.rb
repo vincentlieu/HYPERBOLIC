b90316c287e88d076c4da32c596a194102c43bdd
vincentlieu <v.lieu93@gmail.com>
2020-05-20 15:05:43 +1000

    Fixed issue with being unable to remove user or listing

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
 
   has_many :listings, dependent: :destroy
 
-  has_many :favourite_listings
+  has_many :favourite_listings, dependent: :destroy
   has_many :favourites, through: :favourite_listings, source: :listing
 
-  has_many :purchases
+  has_many :purchases, dependent: :destroy
 
   validates :full_name, presence: true
 end
